a2dfbabdef25d475d882158ae0ce7b47fa0bdf19
TharakaWijesundara <wmtharaka98@gmail.com>
2021-11-17 01:13:16 +0530

    added other functions to vote

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -3,9 +3,31 @@ pragma solidity ^0.6.0;
 
 contract Election{
 
-    string public candidate;
+    struct Candidate{
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(address => bool) public voters;
+    mapping(uint => Candidate) public candidates;
+
+    uint public candidatesCount;
 
     constructor() public{
-        candidate = "candidate1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint _candidateId) public {
+        require(!voters[msg.sender]);
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount ++;
     }
 }
